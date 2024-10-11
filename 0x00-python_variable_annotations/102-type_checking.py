b55022347256f18cec7b66c3ad01b32941718a88
harper71 <princeazuka99@gmail.com>
2024-10-11 06:00:47 +0100

    commit cahnhe

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
-
 """Module that contains a type-annotated function zoom_array
 that takes a tuple of integers and returns a tuple of integers
 with a factor of 2 or 3.0.
 
 """
-
-
-# import Tuple class from the typing module
 from typing import Tuple
 
 
