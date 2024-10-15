3a00ca9602480e74a6f66371ab871707f3467ab5
harper71 <princeazuka99@gmail.com>
2024-10-15 23:35:45 +0100

    task1

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -20,6 +20,5 @@ async def async_generator() -> AsyncGenerator[float, None]:
     """
 
     for _ in range(10):
-        newNums: float = random.uniform(0, 10)
         await asyncio.sleep(1)
-        yield newNums
+        yield random.uniform(0, 10)
